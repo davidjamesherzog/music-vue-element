5b39880ef5d7d9809dd4940500658cbee1902ba1
Dave Herzog <dherzog@dramafever.com>
2019-10-01 18:27:19 -0400

    setup home page

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container style="height: 100%">
+      <el-header>
+        <router-link
+        to='/'
+        tag='span'
+        style='cursor: pointer'>
+        Music
+        </router-link>
+        <span class="float-right">
+          <el-input placeholder="Search Albums" v-model="searchString">
+            <el-button slot="append" icon="el-icon-search" :disabled="!dataAvailable" @click="searchMusic"></el-button>
+          </el-input>
+        </span>  
+      </el-header>
+      
+
+      <el-main>
+        Place Holder
+      </el-main>
+    </el-container>
+    
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+@Component({
+  name: 'App'
+})
+export default class App extends Vue {
+
+  private searchString: string = '';
+
+  public searchMusic() {
+    console.log(`SEARCH MUSIC - ${this.searchString}`);
+    // this.$router.push('/search/' + this.searchString);
+    // this.searchString = '';
+  }
+
+  get dataAvailable() {
+    return this.searchString !== null && this.searchString !== '';
   }
 }
 </script>
@@ -29,8 +51,35 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
+}
+
+.float-right {
+  float: right;
+}
+
+.el-header {
+  overflow: hidden;
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+  font-size: 1rem;
+  position: fixed;
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+  z-index: 9999;
+}
+
+.el-main {
   margin-top: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+
+body {
+  margin: 0px
 }
 </style>
